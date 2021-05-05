206432e2aebf4a156226378271fb04c1e8cdf064
Tyler Hoersch <thoersch@gmail.com>
2021-05-05 09:22:30 -0500

    switched views to use generic api views

--- TodoListAPI/API/views.py
+++ TodoListAPI/API/views.py
@@ -1,52 +1,13 @@
-from rest_framework import status
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from django.http import Http404
+from rest_framework import generics
 from API.models import Todo
 from API.serializers import TodoSerializer
 
 
-class TodoList(APIView):
-    """
-    List or create todos
-    """
-    def get(self, request, format = None):
-        todos = Todo.objects.all()
-        serializer = TodoSerializer(todos, many = True)
-        return Response(serializer.data)
-
-    def post(self, request, format = None):
-        serializer = TodoSerializer(data = request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status = status.HTTP_201_CREATED)
-        return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+class TodoList(generics.ListCreateAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
 
 
-class TodoDetail(APIView):
-    """
-    CRUD on a todo
-    """
-    def get_object(self, pk):
-        try:
-            todo = Todo.objects.get(pk = pk)
-        except Todo.DoesNotExist:
-            raise Http404
-
-    def get(self, request, pk, format = None):
-        todo = self.get_object(pk)
-        serializer = TodoSerializer(todo)
-        return Response(serializer.data)
-
-    def put(self, request, pk, format = None):
-        todo = self.get_object(pk)
-        serializer = TodoSerializer(todo, data = request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk, format = None):
-        todo = self.get_object(pk)
-        todo.delete()
-        return Response(status = status.HTTP_204_NO_CONTENT)
+class TodoDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
